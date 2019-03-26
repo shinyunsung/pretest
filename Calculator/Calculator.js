603bb8752a34828002eb16e388b39f391e5b568e
shinyunsung <sinyunsung@naver.com>
2019-03-26 23:49:33 +0900

    js적용/ clear() 기능오류 수정필요

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -1,5 +1,15 @@
 
-function add(char) {
-    var input = document.getElementById("input");
-    input.value += char;
+function add(n) {
+    var result = document.getElementById("result");
+    result.value = result.value + n;
+}
+
+function clear() {  //기능 작동이 안함
+    var result = document.getElementById("result");
+    result.value = "";
+}
+
+function calc() {
+    var result = document.getElementById("result");
+    result.value = eval(result.value);
 }
